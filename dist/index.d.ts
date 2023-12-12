e59420ead29bf0c857386611137c543e6a842a91
amir504036 <amir504036@gmail.com>
2023-12-12 23:14:59 +0330

    fix problem44

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { AbstractApiData } from "./jsonApi/abstracts/AbstractApiData";
-export { ApiDataFactory } from "./jsonApi/factories/ApiDataFactory";
-export { ApiDataInterface } from "./jsonApi/interfaces/ApiDataInterface";
-export { ApiResponseInterface } from "./jsonApi/interfaces/ApiResponseInterface";
-export { JsonApiHydratedDataInterface } from "./jsonApi/interfaces/JsonApiHydratedDataInterface";
--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-// index.ts
-var greet = function (name) {
-    return "Hello, ".concat(name, "!");
-};
-exports.greet = greet;
