0b27c177725f4c093f0fec237d4155d79884e781
amir504036 <amir504036@gmail.com>
2023-12-12 23:15:52 +0330

    fix problem 35344

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+exports.greet = greet;
+//# sourceMappingURL=index.js.map
