57f30962f884856ba520c2bde7ea30524910949a
amir504036 <amir504036@gmail.com>
2023-12-12 20:23:01 +0330

    phase 2

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts', // assuming your TypeScript files are in the 'src' directory
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
